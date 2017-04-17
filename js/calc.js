d06938a2514588b3d2ea7341ef8adadc604bcd6e
Moses Franco <mosesf@outlook.com>
2017-04-17 12:30:08 -0500

    Added some targets

--- js/calc.js
+++ js/calc.js
@@ -3,5 +3,10 @@
  */
 "use strict";
 (function () {
+var leftOperand = document.getElementById('leftOp');
+var rightOperand = document.getElementById('rightOp');
+
+var operrator = document.getElementById('operator');
+
 
 })();
