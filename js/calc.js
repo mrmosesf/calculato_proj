97626c2b9bef7c097232028b61bd614c224f37a1
cstoker <stoker.cara@gmail.com>
2017-04-17 14:00:22 -0500

    added more stuff

--- js/calc.js
+++ js/calc.js
@@ -8,5 +8,30 @@ var rightOperand = document.getElementById('rightOp');
 
 var operrator = document.getElementById('operator');
 
+var one = document.getElementById('btnOne');
+var two = document.getElementById('btnTwo');
+var three = document.getElementById('btnThree');
+var four = document.getElementById('btnFour');
+var five = document.getElementById('btnFive');
+var six = document.getElementById('btnSix');
+var seven = document.getElementById('btnSeven');
+var eight = document.getElementById('btnEight');
+var nine = document.getElementById('btnTen');
+var zero = document.getElementById('btnZero');
+
+var mult = document.getElementById('btnMult');
+var clear = document.getElementById('btnClear');
+var equal = document.getElementById('btnEqual');
+var divide = document.getElementById('btnDivide');
+var plus = document.getElementById('btnPlus');
+var minus = document.getElementById('btnMinus');
+
+function pressOne() {
+
+}
+
+one.addEventListener('click', pressOne);
+
+var placeholder = '0';
 
 })();
